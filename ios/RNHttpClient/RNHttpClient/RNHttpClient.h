77d630139a2a4158a85922d15bd0c7858407302d
dev.comigo <dev.comigo@gmail.com>
2025-07-28 17:03:32 -0300

    feat: old architecture

---
+++ ios/RNHttpClient/RNHttpClient/RNHttpClient.h
@@ -0,0 +1,12 @@
+//
+//  RNHttpClient.h
+//  RNHttpClient
+//
+//  Created by Comigo on 28/07/25.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNHttpClient : NSObject
+
+@end
---
+++ ios/RNHttpClient/RNHttpClient/RNHttpClient.m
@@ -0,0 +1,12 @@
+//
+//  RNHttpClient.m
+//  RNHttpClient
+//
+//  Created by Comigo on 28/07/25.
+//
+
+#import "RNHttpClient.h"
+
+@implementation RNHttpClient
+
+@end
